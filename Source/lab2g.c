fb5d0b5e934e393edf368996ebf415d54def5105
leanhhuy <anhhuy@live.com>
2014-04-08 19:02:52 +0700

    fix bug: 005

--- Source/lab2g.c
+++ Source/lab2g.c
@@ -6,15 +6,6 @@
  */
 #include "control.h"
 
-int soog[10] = { 0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90 };
-
-void show(int a, int t)
-{
-	P2OUT = soog[a];
-	P1OUT = t;
-	__delay_cycles(10000);
-	P1OUT &=~ t;
-}
 int lab2g(void){
 	P1DIR |= 0xF0;
 	P1REN |= 0x04;	////PxREN = 0; Pullup/pulldown resistor disable
@@ -31,11 +22,11 @@ int lab2g(void){
 					a++;
 					if(a == 99) a=0;
 				}
-				show(a/10,T1);
-				show(a%10,T2);
+				showled(a/10,T1);
+				showled(a%10,T2);
 			}
-			show(a/10,T1);
-			show(a%10,T2);
+			showled(a/10,T1);
+			showled(a%10,T2);
 		}
 }
 
